11c8d5bd65a2defe6fd2e2af8f9fd2853a36d5d6
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-12 15:42:25 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -17,3 +17,21 @@ plt.show()
 
 
 
+import pandas as pd
+import matplotlib.pyplot as plt
+# Read data from CSV file
+data = pd.read_csv('germination_speed.csv')
+# Calculate speed of germination
+data['Speed of Germination'] = data['Num Germinated'].diff()
+data.loc[0, 'Speed of Germination'] = data.loc[0, 'Num Germinated'] # Fix for warning
+# Visualize the results
+fig, ax = plt.subplots()
+ax.set_xlabel('Day')
+ax.set_ylabel('Number of Seeds Germinated')
+ax.plot(data['Day'], data['Num Germinated'], label='Number of Seeds Germinated')
+ax2 = ax.twinx()
+ax2.set_ylabel('Speed of Germination')
+ax2.plot(data['Day'], data['Speed of Germination'], color='red', label='Speed of Germination')
+plt.title('Germination Analysis')
+plt.legend([ax.get_lines()[0], ax2.get_lines()[0]], ['Number of Seeds Germinated', 'Speed of Germination'])
+plt.show()
